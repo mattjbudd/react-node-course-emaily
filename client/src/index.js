5e3bcc21a8a7119317570c2031a0bcc6e8589f30
BuildTools <unconfigured@null.spigotmc.org>
2018-12-31 16:46:24 -0700

    lots of stuff

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,21 @@ import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+// Development only axios helpers
+import axios from 'axios';
+window.axios = axios;
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(reduxThunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
